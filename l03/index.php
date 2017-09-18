c115673670e9810b3a1e8ec5ba99d1fc6dd83537
Trần Hữu Thiện <thienth32@gmail.com>
2017-09-18 10:37:55 +0700

    update index

--- l03/index.php
+++ l03/index.php
@@ -3,11 +3,53 @@ require_once 'models/User.php';
 require_once 'models/Product.php';
 
 // lay toan bo du lieu tu bang user thoa man dieu kien id > 0
-$users = User::findOne(1);
-echo "<pre>";
-var_dump($users);
+$products = Product::all();
 
-echo "danh sach san pham duoc tao boi user nay la: ";
-echo "<pre>";
-var_dump($users->getOwnProduct());
+// echo "<pre>";
+// var_dump($users);
+
+// echo "danh sach san pham duoc tao boi user nay la: ";
+// echo "<pre>";
+// var_dump($users->getOwnProduct());
  ?>
+
+ <table>
+ 	<thead>
+ 		<tr>
+ 			<th>ID</th>
+ 			<th>Name</th>
+ 			<th>Detail</th>
+ 			<th>Price</th>
+ 			<th>Owner</th>
+ 		</tr>
+ 	</thead>
+ 	<tbody>
+ 		<?php 
+ 		foreach ($products as $pro) {
+
+
+ 			?>
+ 			<tr>
+	 			<th>
+	 				<?= $pro->id?>
+	 			</th>
+	 			<th>
+	 				<?= $pro->name?>
+	 			</th>
+	 			<th>
+	 				<?= $pro->detail?>
+	 			</th>
+	 			<th>
+	 				<?= $pro->sell_price?>
+	 			</th>
+	 			<th>
+	 				<?= $pro->owner()->name?>
+	 			</th>
+	 		</tr>
+ 			<?php
+ 		}
+
+ 		 ?>
+ 		
+ 	</tbody>
+ </table>
